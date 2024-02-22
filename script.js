29e17008b178f82c96939a4b224bbce7c7073eec
vanessalayson <vanessalayson25@gmail.com>
2024-02-23 07:27:48 +0800

    Test 003 white. Updates are img border radius, nav modal automatically closes when link is clicked, added space between preview icon and text

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 // HAMBURGER MENU
 const menuToggle = document.getElementById("menu-toggle");
 const navbarLinks = document.getElementById("navbar-links");
+const closeModal = document.getElementById("closeModal");
+const modal = document.getElementById("myModal");
 
 menuToggle.addEventListener("click", () => {
   navbarLinks.classList.toggle("show");
   menuToggle.classList.toggle("change");
+});
+
+navbarLinks.querySelectorAll(".nav-link").forEach((link) => {
+  link.addEventListener("click", () => {
+    navbarLinks.classList.remove("show");
+    menuToggle.classList.remove("change");
+    modal.style.display = "none";
+  });
+});
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
 });
 
 window.addEventListener("scroll", () => {
